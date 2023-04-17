458ae1f1acb7633576feff819749f3618f69e742
hjkim2040 <hjkim2661@gmail.com>
2023-04-17 13:40:00 +0900

    교점 구현

--- src/main/java/com/ll/level2/p87377/Main.java
+++ src/main/java/com/ll/level2/p87377/Main.java
@@ -2,7 +2,7 @@ package com.ll.level2.p87377;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Solution().intersection());
+        System.out.println(new Solution().intersection(new int[]{1, -1, 0}, new int []{2, -1 ,0}));
 
     }
 }
@@ -12,7 +12,25 @@ class Solution {
         String[] answer = {};
         return answer;
     }
-    public int [] intersection(int[] a,int[] b){
+    public long [] intersection(int[] line1,int[] line2){
+        long A = line1[0];
+        long B = line1[1];
+        long E = line1[2];
 
+        long C = line2[0];
+        long D = line2[1];
+        long F = line2[2];
+
+        if ((A * D - B * C) == 0){
+            return null;
+        }
+
+
+        double x = (double)(B * F - E * D) / (A * D - B * C);
+        double y = (double)(E * C - A * F) / (A * D - B * C);
+
+        if(x != (long)x){return null;}
+        if(y != (long)y){return null;}
+            return new long[]{(long)x,(long)y};
     }
 }
