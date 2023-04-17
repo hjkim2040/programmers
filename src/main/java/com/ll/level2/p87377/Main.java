0bb0ac04ff72344962f2de17ea4d239b2a7b168d
hjkim2040 <hjkim2661@gmail.com>
2023-04-17 13:10:49 +0900

    실패하는 테스트 만들기

---
+++ src/main/java/com/ll/level2/p87377/Main.java
@@ -0,0 +1,18 @@
+package com.ll.level2.p87377;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().intersection());
+
+    }
+}
+
+class Solution {
+    public String[] solution(int[][] line) {
+        String[] answer = {};
+        return answer;
+    }
+    public int [] intersection(int[] a,int[] b){
+
+    }
+}
