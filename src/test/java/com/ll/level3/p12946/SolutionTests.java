e1854292904dfdcfbcee8de09e8a3d183d5c06e8
hjkim2040 <hjkim2661@gmail.com>
2023-04-19 11:21:53 +0900

    하노이 탑 시작

---
+++ src/test/java/com/ll/level3/p12946/SolutionTests.java
@@ -0,0 +1,32 @@
+package com.ll.level3.p12946;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class SolutionTests {
+    @Test
+    @DisplayName("n=1 => [[1,3]]")
+    void t01() {
+        assertThat(
+                new Solution().solution(1)
+        ).isEqualTo(
+                new int[][]{{1, 3}}
+        );
+    }
+
+    @Test
+    @DisplayName("n=2 => [[1,2], [1,3], [2,3]]")
+    void t02() {
+        assertThat(
+                new Solution().solution(2)
+        ).isEqualTo(
+                new int[][]{{1, 2}, {1, 3}, {2, 3}}
+        );
+    }
+}
