8307de3d62292fb1d8eae22fa6065ef22279d0a9
hjkim2040 <hjkim2661@gmail.com>
2023-04-19 13:11:29 +0900

    디버그

--- src/main/java/com/ll/level3/p12946/Main.java
+++ src/main/java/com/ll/level3/p12946/Main.java
@@ -17,20 +17,27 @@ class Hanoi {
 
     public Hanoi(int from, int to, int n) {
 
-        calculate(from, to, n);
+        calculate(from, to, n, 1);
     }
 
-    public void calculate(int from, int to, int n) {
+    public void calculate(int from, int to, int n, int depth) {
+
+        String debugLineHead = "\t".repeat(depth - 1) + "(%d) => (%d), %d개 옮기기".formatted(from, to, n);
+
+        System.out.println(debugLineHead + " - 시작");
 
 
         if (n == 1) {
             addPath(from, to);
+            System.out.println(debugLineHead + " - 끝");
             return;
         }
         int empty = 6 - from -to;
-        calculate(from,empty,n-1);
-        calculate(from,to,1);
-        calculate(empty,to,n-1);
+        calculate(from,empty,n-1, depth + 1);
+        calculate(from,to,1, depth + 1);
+        calculate(empty,to,n-1, depth + 1);
+
+        System.out.println(debugLineHead + " - 끝");
 
     }
     private void addPath(int from, int to){
