5869f24ba4687f5216b3242dc4af5c312630b2ba
hjkim2040 <hjkim2661@gmail.com>
2023-04-21 16:04:08 +0900

    조이스틱 시작

---
+++ src/main/java/com/ll/level2/p42860/Main.java
@@ -0,0 +1,13 @@
+package com.ll.level2.p42860;
+
+public class Main {
+}
+
+class Solution{
+    public static boolean isDebug = false;
+
+    public int solution(String name) {
+        return 1;
+    }
+
+}
