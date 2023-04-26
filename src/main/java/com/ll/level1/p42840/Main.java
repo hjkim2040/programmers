a576f960d575d5d409fc32704932d260b2529c16
hjkim2040 <hjkim2661@gmail.com>
2023-04-26 10:15:41 +0900

    모의고사 문제

---
+++ src/main/java/com/ll/level1/p42840/Main.java
@@ -0,0 +1,10 @@
+package com.ll.level1.p42840;
+
+public class Main {
+}
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        return answer;
+    }
+}
---
+++ src/test/java/com/ll/level1/p42840/SolutionTests.java
@@ -0,0 +1,7 @@
+package com.ll.level1.p42840;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTests {
+
+}
