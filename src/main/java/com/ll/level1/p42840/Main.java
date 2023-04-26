e9c40683b2ae189980c2108ee2f0a00178e39fac
hjkim2040 <hjkim2661@gmail.com>
2023-04-26 11:37:42 +0900

    성공

--- src/main/java/com/ll/level1/p42840/Main.java
+++ src/main/java/com/ll/level1/p42840/Main.java
@@ -1,6 +1,7 @@
 package com.ll.level1.p42840;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
@@ -28,7 +29,10 @@ class Solution {
                 .max()
                 .getAsInt();
 
-        return new int[]{maxPoint};
+        // 각각의 방법으로 했을 때의 점수를 얻은 후 그것이 최고점수이면 배열에 수포자 번호 담는다.
+        return IntStream.rangeClosed(1, 3)
+                .filter(patterNo -> maxPoint == scoreOf(answers, patterNo))
+                .toArray();
     }
 
     private int scoreOf1(int[] answers) {
@@ -41,6 +45,12 @@ class Solution {
 
     private int scoreOf3(int[] answers) {
         return scoreOf(answers, new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5});
+    }
+    private int scoreOf(int[] answers, int patternNo) {
+        if (patternNo == 1) return scoreOf1(answers);
+        if (patternNo == 2) return scoreOf2(answers);
+
+        return scoreOf3(answers);
     }
 
     private int scoreOf(int[] answers, int[] pattern) {
