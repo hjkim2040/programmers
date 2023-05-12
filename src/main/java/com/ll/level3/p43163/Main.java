ce9dddca5102ddcd3c874adb881c7dfa9f4fe391
hjkim2040 <hjkim2661@gmail.com>
2023-05-12 16:35:24 +0900

    p43163

---
+++ src/main/java/com/ll/level3/p43163/Main.java
@@ -0,0 +1,10 @@
+package com.ll.level3.p43163;
+
+public class Main {
+}
+class Solution {
+    public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ src/test/java/com/ll/level3/p43163/SolutionTests.java
@@ -0,0 +1,7 @@
+package com.ll.level3.p43163;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTests {
+
+}
