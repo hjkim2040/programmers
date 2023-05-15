ecfde493601c16fa4587c17c4b0c8ed4f8741775
hjkim2040 <hjkim2661@gmail.com>
2023-05-15 13:47:51 +0900

    p42583

---
+++ src/main/java/com/ll/level2/p42583/Main.java
@@ -0,0 +1,9 @@
+package com.ll.level2.p42583;
+
+public class Main {
+}
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        return 101;
+    }
+}
