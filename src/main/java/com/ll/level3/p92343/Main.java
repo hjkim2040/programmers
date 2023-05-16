aa17e347a7a1bdd5e3c1dec1ff59e8920d14175d
hjkim2040 <hjkim2661@gmail.com>
2023-05-16 09:46:03 +0900

    p92343

---
+++ src/main/java/com/ll/level3/p92343/Main.java
@@ -0,0 +1,10 @@
+package com.ll.level3.p92343;
+
+public class Main {
+}
+class Solution {
+    public int solution(int[] info, int[][] edges) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ src/test/java/com/ll/level3/p92343/SolutionTests.java
@@ -0,0 +1,7 @@
+package com.ll.level3.p92343;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTests {
+
+}
